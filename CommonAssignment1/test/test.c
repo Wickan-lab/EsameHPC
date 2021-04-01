31879240900f65552850b89c5950be61ab1205c7
Wickan-lab <g.capitani@studenti.unisa.it>
2021-04-01 16:22:59 +0200

    test c

--- CommonAssignment1/test/test.c
+++ CommonAssignment1/test/test.c
@@ -23,7 +23,8 @@ int main(int argc, char const *argv[])
 {
 	double *a, *b, *result;
 	int threads = 4;
-	int columns,rows = 10;
+	int columns = 10;
+	int rows = 10;
 	init_structures(&a, &b, &result, rows, columns, threads);
 	test_init_structure(a,(rows*columns));
 	test_init_structure(b,columns);
@@ -37,41 +38,48 @@ int main(int argc, char const *argv[])
 	double b1[columns];
 	double b2[columns];
 	double b3[columns];
+	double b4[columns];
 
 	double result1[rows]; 
 	double result3[rows]; 
 	double result2[rows]; 
 
-	double expected_result1[] = {0,0,0,0,0,0,0,0,0,0};
+	double expected_result4[] = {20,40,60,80,100,120,140,160,180,200};
 	double expected_result2[] = {385,385,385,385,385,385,385,385,385,385};
 	double expected_result3[] = {1,2,3,4,5,6,7,8,9,10};
+	double expected_result1[] = {0,0,0,0,0,0,0,0,0,0};
 
-	for(int i=0; i<(rows*columns);i++){
+	for(int i=0; i<(rows);i++){
 		for(int j = 0; j < columns; j++){
 			a1[i*columns + j] = (double)(i + 1);
 			a2[i*columns + j] = (double)(j + 1);
 			if(i == j)
-				a3[i*columns + j] = (double)i; 
+				a3[i*columns + j] = (double)(i + 1); 
 			else
 				a3[i*columns + j] = 0.0;
 
 		}
+
 	}
 
 	for(int k = 0; k < columns; k++){
 		b1[k] = 0.0;
 		b2[k] = (double)(k + 1);
 		b3[k] = 1.0;
+		b4[k] = 2.0;
 	}
 	
-	dot_product(a1,b1, result1,rows,columns,threads);
-	test_dot_product(expected_result1,result1,rows);
+	dot_product(a1,b1, result,rows,columns,threads);
+	test_dot_product(expected_result1,result,rows);
 
-	dot_product(a2,b2,result2,rows,columns,threads);
-	test_dot_product(expected_result2,result2,rows);
+	dot_product(a2,b2,result,rows,columns,threads);
+	test_dot_product(expected_result2,result,rows);
 
-	dot_product(a3,b3,result3,rows,columns,threads);
-	test_dot_product(result3, expected_result3,rows);
+	dot_product(a3,b3,result,rows,columns,threads);
+	test_dot_product(result, expected_result3,rows);
+
+	dot_product(a1,b4,result,rows,columns,threads);
+	test_dot_product(result, expected_result4,rows);
 
 	free(a);
 	free(b);
