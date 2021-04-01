fa9a37c506d3cf23bc6bcd96400fd3771b5a9ec3
LuigiTer <luigi.terrone97@gmail.com>
2021-04-01 11:39:36 +0200

    Some changes

--- CommonAssignment1/src/main.c
+++ CommonAssignment1/src/main.c
@@ -4,6 +4,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 #ifdef _OPENMP
 	#include <omp.h>
@@ -11,14 +12,24 @@
 	#define get_thread_num() 0
 #endif
 
+/** macros to get execution time: both macros have to be in the same scope
+*   define a double variable to use in ENDTIME before STARTTIME:
+*   double x;
+*   the variable will hold the execution time in seconds.
+*/
+
+#define STARTTIME(id) clock_t start_time_42_id,end_time_42_id; start_time_42_id = clock()
+
+#define ENDTIME(id,x) end_time_42_id=clock(); x=((double) (end_time_42_id-start_time_42_id)) /  CLOCKS_PER_SEC
+
 /**
  * @brief This function initializes all the data structures needed in the program. 
- * @param *(*a)        pointer to the matrix used in the dot product.
- * @param *(*b)        pointer to the array used in the dot product.
- * @param *(result)    pointer to array used to store the result of dot product.
- * @param rows         number of rows.
- * @param colums       number of columns.
- * @param threads      number of threads.
+ * @param a        pointer to the matrix used in the dot product.
+ * @param b        pointer to the array used in the dot product.
+ * @param result   pointer to array used to store the result of dot product.
+ * @param rows     number of rows.
+ * @param colums   number of columns.
+ * @param threads  number of threads.
  */
 void init_structures(double **a, double **b, double **result, int rows, int columns, int threads){
 
@@ -65,21 +76,19 @@ void init_structures(double **a, double **b, double **result, int rows, int colu
 	*result = tmp_result;
 }
 
-int main(int argc, char const *argv[])
-{	
+/**
+ * @brief This function makes the dot product between the matrix 'a' and the array 'b'. 
+ * @param a          pointer to the matrix used in the dot product.
+ * @param b          pointer to the array used in the dot product.
+ * @param result     pointer to array used to store the result of dot product.
+ * @param rows       number of rows.
+ * @param colums     number of columns.
+ * @param threads    number of threads.
+ */
+void dot_product(double *a, double *b, double *result, int rows, int columns, int threads){
 
-	double dot, *a, *b, *result;
-	int i, j, rows, columns, threads;	
-
-	if(argc < 4){
-		printf("ERROR! Usage: ./main rows columns threads");
-	}
-
-	rows = atoi(argv[1]);
-	columns = atoi(argv[2]);
-	threads = atoi(argv[3]);
-
-	init_structures(&a, &b, &result, rows, columns, threads);
+	int i, j; 
+	double dot;
 
 	#pragma omp parallel for default(none) shared(a, b, result, rows, columns) private (i, j, dot) num_threads(threads)
 		for(i = 0; i < rows; ++i){			
@@ -88,6 +97,29 @@ int main(int argc, char const *argv[])
 				dot += a[i*columns + j]*b[j];			
 			result[i] = dot;
 		}
+}
+
+int main(int argc, char const *argv[])
+{	
+
+	double dot, *a, *b, *result, timefind = 0.0;
+	int i, j, rows, columns, threads;	
+
+	if(argc < 4)
+		printf("ERROR! Usage: ./main rows columns threads");	
+
+	rows = atoi(argv[1]);
+	columns = atoi(argv[2]);
+	threads = atoi(argv[3]);
+
+	STARTTIME(1);
+
+	init_structures(&a, &b, &result, rows, columns, threads);
+	dot_product(a, b, result, rows, columns, threads);
+
+	ENDTIME(1,timefind);
+
+	printf("Time : %f\n", timefind);
 
 	return 0;
 }
