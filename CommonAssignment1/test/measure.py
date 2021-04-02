1880f2adeb4f645914e3497e5b34f482eef23d83
Wickan-lab <g.capitani@studenti.unisa.it>
2021-04-02 17:23:10 +0200

    Update measure.py

--- CommonAssignment1/test/measure.py
+++ CommonAssignment1/test/measure.py
@@ -3,10 +3,10 @@ import subprocess
 import csv
 
 def main():
-	n_measures = 1000
+	n_measures = 10000
 	fieldnames = ['rows', 'columns', 'threads', 'time']  
 	n_threads_cases = ['0', '1', '2', '4', '8']
-	problem_size_cases = [ ['1000', '1000'], , ['10000', '10000'], ['20000', '20000'] ]
+	problem_size_cases = [ ['10000', '10000'] ]
 	results_to_print = []
 	results_to_print.append(fieldnames)	
 
@@ -22,7 +22,7 @@ def main():
 				row_to_write.append(result.stdout.decode('utf-8').strip())
 				results_to_print.append(row_to_write)
 
-			with open('measures_log_test.csv', mode='a') as measures_file:	 
+			with open('test/measures_log'+ time.strftime("%H-%M-%S") + '.csv', mode='w') as measures_file: 
 				measures_writer = csv.writer(measures_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)  
 				measures_writer.writerows(results_to_print)
 
