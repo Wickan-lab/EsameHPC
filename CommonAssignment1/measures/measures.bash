0df3e626bca1626eb97f9334de42229fb516c37b
Armando Falanga <armando.falanga@gmail.com>
2021-04-03 13:23:33 +0200

    [ADD] measures.bash
    Add script for measures

---
+++ CommonAssignment1/measures/measures.bash
@@ -0,0 +1,21 @@
+NMEASURES=5000
+
+ARRAY_RC=(5000 8000)
+ARRAY_THS=(0 1 2 4 8)
+mkdir measure 2> /dev/null 
+
+for ths in "${ARRAY_THS[@]}"; do
+	for size in "${ARRAY_RC[@]}"; do
+		OUT_FILE=measure/SIZE-$size-NTH-$ths-$(date +%s).csv
+		echo $OUT_FILE
+		echo "row,columns,threads,user,elapsed,sys,pCPU" >$OUT_FILE
+		for ((i = 0 ; i < $NMEASURES	; i++)); do
+			if [[ $ths -ne 0 ]]; then 
+				/usr/bin/time -f "$size,$size,$ths,%U,%e,%S,%P" -o $OUT_FILE --append ../build/program $size $size $ths
+			else
+				/usr/bin/time -f "$size,$size,$ths,%U,%e,%S,%P" -o $OUT_FILE --append ../build/program_seq $size $size $ths
+			fi
+		done
+	done
+done
+
