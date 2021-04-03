2f91fe77ab333baec72d4c4db663adae8f11df7e
Armando Falanga <armando.falanga@gmail.com>
2021-04-03 14:11:42 +0200

    [EDIT] measures.bash
    Ctrl-C and progress bar

--- CommonAssignment1/measures/measures.bash
+++ CommonAssignment1/measures/measures.bash
@@ -1,7 +1,9 @@
-NMEASURES=5000
+NMEASURES=50
 
 ARRAY_RC=(5000 8000)
 ARRAY_THS=(0 1 2 4 8)
+
+trap "exit" INT
 mkdir measure 2> /dev/null 
 
 for ths in "${ARRAY_THS[@]}"; do
@@ -15,7 +17,10 @@ for ths in "${ARRAY_THS[@]}"; do
 			else
 				/usr/bin/time -f "$size,$size,$ths,%U,%e,%S,%P" -o $OUT_FILE --append ../build/program_seq $size $size $ths
 			fi
+			printf "\r>%3.1d%% " $(expr \( $i \* 100 \) / $NMEASURES)
+			printf "#%.0s" $(seq -s " " 1 $(expr \( $i \* 40 \) / $NMEASURES))
 		done
+		printf "\n"
 	done
 done
 
