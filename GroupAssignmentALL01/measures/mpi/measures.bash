41fcfb67bd64e2cf20fe2bf6a27f22b423177325
Wickan-lab <g.capitani@studenti.unisa.it>
2021-06-21 18:26:59 +0200

    to do meas

--- GroupAssignmentALL01/measures/mpi/measures.bash
+++ GroupAssignmentALL01/measures/mpi/measures.bash
@@ -30,10 +30,10 @@
 TIME_STAMP=$(date +%s)
 NMEASURES=100
 
-ARRAY_RC=(2097152)
+ARRAY_RC=(1048576 2097152 4194304)
 ARRAY_THS=(0 1 2 4 8 16)
-CLUSTER_NUM=(32)
-ARRAY_K_VALUES=(1024) # mi basta fare mcm dei processi che dato che sono potenze di due viene 16
+CLUSTER_NUM=(32 32 32)
+ARRAY_K_VALUES=(1024 2048 4096) # mi basta fare mcm dei processi che dato che sono potenze di due viene 16
 TIMEFORMAT='%3U;%3E;%3S;%P'
 
 trap "exit" INT
